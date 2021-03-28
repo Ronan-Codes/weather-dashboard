61db2f2413a5bb314ef804c1e1c58aec247bb4b8
Ronan <galv.ronan@gmail.com>
2021-03-28 19:54:49 -0400

    variables for querySelectors declared

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,51 @@
+// querySelectors Picked City Title - date1 - icon1
+var cityContainer = $("#cityContainer");
+var date1 = $("#date1");
+var icon1 = $("#icon1");
 
+// querySelectors Picked City weather info
+var tempContainer1 = $("#tempContainer1");
+var humidityContainer1 = $("#humidityContainer1");
+var windContainer = $("#windContainer");
+var uvContainer = $("#uvContainer");
+
+// querySelectors 5-Day Forecast: date2
+var date1 = $("#date2");
+var icon1 = $("#icon2");
+var tempContainer1 = $("#tempContainer2");
+var humidityContainer1 = $("#humidityContainer2");
+
+// querySelectors 5-Day Forecast: date3
+var date1 = $("#date3");
+var icon1 = $("#icon3");
+var tempContainer1 = $("#tempContainer3");
+var humidityContainer1 = $("#humidityContainer3");
+
+// querySelectors 5-Day Forecast: date4
+var date1 = $("#date4");
+var icon1 = $("#icon4");
+var tempContainer1 = $("#tempContainer4");
+var humidityContainer1 = $("#humidityContainer4");
+
+// querySelectors 5-Day Forecast: date5
+var date1 = $("#date5");
+var icon1 = $("#icon5");
+var tempContainer1 = $("#tempContainer5");
+var humidityContainer1 = $("#humidityContainer5");
+
+// querySelectors 5-Day Forecast: date6
+var date1 = $("#date6");
+var icon1 = $("#icon6");
+var tempContainer1 = $("#tempContainer6");
+var humidityContainer1 = $("#humidityContainer6");
+
+// querySelectors for City Form
+var cityInput = $("#cityInput");
+var cityBtn = $("#cityBtn");
+var citiesContainer = $("#citiesContainer");
+
+// Fetch Functions Start
+
+// Fetch Functions End
+
+// Click Events (citiesBtn & cityLi[data-city: #])
