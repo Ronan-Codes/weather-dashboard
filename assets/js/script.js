cbc303bb9957d8e1a4344878a1d3a2c04601dd40
Ronan <galv.ronan@gmail.com>
2021-03-29 16:28:38 -0400

    5-Day Forecast Cards Functions Solved

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,98 @@ var cityInput = $("#cityInput");
 var cityBtn = $("#cityBtn");
 var citiesContainer = $("#citiesContainer");
 
-// Fetch Functions Start
+var currentDay = moment().format("MM-DD-YYYY");
 
+
+// Fetch Functions Start -> leads to displayWeather
+var fetchWeather = function(pickedCity) {
+    // API URL for Picked City
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather/?q=" + pickedCity + "&appid=86fcb44b6b11593f53514dda5d0a62ae";
+
+    fetch(apiUrl).then(function(response) {
+        // check if request was successful
+        if(response.ok) {
+            response.json().then(function(data) {
+
+                var uvApiUrl = "http://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon +"&appid=86fcb44b6b11593f53514dda5d0a62ae"
+                fetch(uvApiUrl).then(function(response) {
+                    if(response.ok) {
+                        response.json().then(function(data) {
+                            // console.log(currentDay)
+                            // console.log(data.current.weather[0].icon);
+                            // console.log(data.current.temp)
+                            // console.log(data.current.humidity)
+                            // console.log(data.current.wind_speed)
+                            // console.log(data.current.uvi)
+                            // console.log(data)
+                            debugger;
+                            for (var i = 1; i < 6; i++) {
+                                var forecastCard = $(".forecastCard[data-day='"+i+"']");
+
+                                var cardDate = moment().add(i, 'days').format("MM-DD-YYYY");
+                                // console.log(cardDate);
+                                var forecastDate = document.createElement("p");
+                                forecastDate.classList = "card-title";
+                                forecastDate.textContent = cardDate;
+                                forecastDate.setAttribute("data-day", i);
+                                forecastCard.append(forecastDate);
+
+                                // console.log(data.daily[i].weather[0].icon)
+                                var forecastIcon = document.createElement("img");
+                                forecastIcon.classList = "card-text";
+                                forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon +".png");
+                                forecastIcon.setAttribute("data-day", i);
+                                forecastCard.append(forecastIcon);
+
+                                // console.log(data.daily[i].temp.day)
+                                var forecastTemp = document.createElement("p");
+                                forecastTemp.classList = "card-text";
+                                forecastTemp.textContent = data.daily[i].temp.day;
+                                forecastTemp.setAttribute("data-day", i);
+                                forecastCard.append(forecastTemp);
+
+                                // console.log(data.daily[i].humidity)
+                                var forecastHumidity = document.createElement("p");
+                                forecastHumidity.classList = "card-text";
+                                forecastHumidity.textContent = data.daily[i].humidity;
+                                forecastHumidity.setAttribute("data-day", i);
+                                forecastCard.append(forecastHumidity);
+                            }
+                        })
+                    }
+                }).catch(function(error) {
+                    alert("Error: " + response.statusText)
+                })
+                // displayWeather(data, pickedCity);
+            }).catch(function(error) {
+                alert("Error: " + response.statusText)
+            })
+        }
+    })
+
+    // var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + pickedCity + "&cnt=6&appid=86fcb44b6b11593f53514dda5d0a62ae";
+    // fetch(fiveDayUrl).then(function(response) {
+    //     if(response.ok) {
+    //         response.json().then(function(data) {
+    //             console.log(data)
+    //         })
+    //     }
+    // })
+}
 // Fetch Functions End
 
+// Form Submission -> leads to fetchWeather
+var formSubmitHandler = function(pickedCity) {};
+
+// Clicked City from Saved List -> leads to fetchWeather
+var clickCity = function(pickedCity) {};
+
+// displayWeather Function
+var displayWeather = function(data, pickedCity) {};
+
 // Click Events (citiesBtn & cityLi[data-city: #])
+
+fetchWeather("atlanta")
+
+// Weather API ID (DELETE COMMENT PRIOR TO SUBMISSION)
+// 86fcb44b6b11593f53514dda5d0a62ae
