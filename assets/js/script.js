91dbe8dd3099db8dac8d70893e1f6ac7aa7c68ab
Ronan <galv.ronan@gmail.com>
2021-03-30 17:20:33 -0400

    clear contents before displaying new city weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,48 +5,51 @@ var currentIcon = document.querySelector("#currentIcon")
 var currentTemp = document.querySelector("#currentTemp")
 var currentHumidity = document.querySelector("#currentHumidity")
 var currentWind = document.querySelector("#currentWind")
-var currentUvi = document.querySelector("#currentUvi");
+var currentUvi = document.querySelector("#currentUvi")
 
-// querySelectors Picked City weather info
-var tempContainer1 = $("#tempContainer1");
-var humidityContainer1 = $("#humidityContainer1");
-var windContainer = $("#windContainer");
-var uvContainer = $("#uvContainer");
+// Submit vars
+var cityFormEl = document.querySelector("#cityForm")
+var cityInputEl = document.querySelector("#cityInput")
 
-// querySelectors 5-Day Forecast: date2
-var date1 = $("#date2");
-var icon1 = $("#icon2");
-var tempContainer1 = $("#tempContainer2");
-var humidityContainer1 = $("#humidityContainer2");
+var displayToggleEl = document.querySelector("#displayToggle");
+// displayToggleEl.style.display = "none";
 
-// querySelectors 5-Day Forecast: date3
-var date1 = $("#date3");
-var icon1 = $("#icon3");
-var tempContainer1 = $("#tempContainer3");
-var humidityContainer1 = $("#humidityContainer3");
+// // querySelectors Picked City weather info
+// var tempContainer1 = $("#tempContainer1");
+// var humidityContainer1 = $("#humidityContainer1");
+// var windContainer = $("#windContainer");
+// var uvContainer = $("#uvContainer");
 
-// querySelectors 5-Day Forecast: date4
-var date1 = $("#date4");
-var icon1 = $("#icon4");
-var tempContainer1 = $("#tempContainer4");
-var humidityContainer1 = $("#humidityContainer4");
+// // querySelectors 5-Day Forecast: date2
+// var date1 = $("#date2");
+// var icon1 = $("#icon2");
+// var tempContainer1 = $("#tempContainer2");
+// var humidityContainer1 = $("#humidityContainer2");
 
-// querySelectors 5-Day Forecast: date5
-var date1 = $("#date5");
-var icon1 = $("#icon5");
-var tempContainer1 = $("#tempContainer5");
-var humidityContainer1 = $("#humidityContainer5");
+// // querySelectors 5-Day Forecast: date3
+// var date1 = $("#date3");
+// var icon1 = $("#icon3");
+// var tempContainer1 = $("#tempContainer3");
+// var humidityContainer1 = $("#humidityContainer3");
 
-// querySelectors 5-Day Forecast: date6
-var date1 = $("#date6");
-var icon1 = $("#icon6");
-var tempContainer1 = $("#tempContainer6");
-var humidityContainer1 = $("#humidityContainer6");
+// // querySelectors 5-Day Forecast: date4
+// var date1 = $("#date4");
+// var icon1 = $("#icon4");
+// var tempContainer1 = $("#tempContainer4");
+// var humidityContainer1 = $("#humidityContainer4");
 
-// querySelectors for City Form
-var cityInput = $("#cityInput");
-var cityBtn = $("#cityBtn");
-var citiesContainer = $("#citiesContainer");
+// // querySelectors 5-Day Forecast: date5
+// var date1 = $("#date5");
+// var icon1 = $("#icon5");
+// var tempContainer1 = $("#tempContainer5");
+// var humidityContainer1 = $("#humidityContainer5");
+
+// // querySelectors 5-Day Forecast: date6
+// var date1 = $("#date6");
+// var icon1 = $("#icon6");
+// var tempContainer1 = $("#tempContainer6");
+// var humidityContainer1 = $("#humidityContainer6");
+
 
 var currentDay = moment().format("MM-DD-YYYY");
 
@@ -91,6 +94,7 @@ var fetchWeather = function(pickedCity) {
 
                             for (var i = 1; i < 6; i++) {
                                 var forecastCard = $(".forecastCard[data-day='"+i+"']");
+                                forecastCard.empty();
 
                                 var cardDate = moment().add(i, 'days').format("MM-DD-YYYY");
                                 // console.log(cardDate);
@@ -122,6 +126,8 @@ var fetchWeather = function(pickedCity) {
                                 forecastHumidity.textContent = data.daily[i].humidity;
                                 forecastHumidity.setAttribute("data-day", i);
                                 forecastCard.append(forecastHumidity);
+
+                                displayToggleEl.classList.remove("d-none");
                             }
                         })
                     }
@@ -135,19 +141,25 @@ var fetchWeather = function(pickedCity) {
         }
     })
 
-    // var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + pickedCity + "&cnt=6&appid=86fcb44b6b11593f53514dda5d0a62ae";
-    // fetch(fiveDayUrl).then(function(response) {
-    //     if(response.ok) {
-    //         response.json().then(function(data) {
-    //             console.log(data)
-    //         })
-    //     }
-    // })
+
 }
 // Fetch Functions End
 
 // Form Submission -> leads to fetchWeather
-var formSubmitHandler = function(pickedCity) {};
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get the value from input element
+    var city = cityInputEl.value.toLowerCase().trim()
+
+
+    if (city) {
+        fetchWeather(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a city.")
+    }
+};
 
 // Clicked City from Saved List -> leads to fetchWeather
 var clickCity = function(pickedCity) {};
@@ -156,8 +168,8 @@ var clickCity = function(pickedCity) {};
 var displayWeather = function(data, pickedCity) {};
 
 // Click Events (citiesBtn & cityLi[data-city: #])
+cityFormEl.addEventListener("submit", formSubmitHandler);
 
-fetchWeather("ocala")
 
 // Weather API ID (DELETE COMMENT PRIOR TO SUBMISSION)
 // 86fcb44b6b11593f53514dda5d0a62ae
