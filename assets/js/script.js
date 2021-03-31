b2e651956347fbc41f1a89d12022c8b6e128cf9c
Ronan <galv.ronan@gmail.com>
2021-03-31 09:43:47 -0400

    Trial to fix Mixed Content Error on GitHub Pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,43 +18,6 @@ var displayToggleEl = document.querySelector("#displayToggle");
 var searchedCitiesEl = document.querySelector("#searchedCities")
 var savedCities = [];
 
-// // querySelectors Picked City weather info
-// var tempContainer1 = $("#tempContainer1");
-// var humidityContainer1 = $("#humidityContainer1");
-// var windContainer = $("#windContainer");
-// var uvContainer = $("#uvContainer");
-
-// // querySelectors 5-Day Forecast: date2
-// var date1 = $("#date2");
-// var icon1 = $("#icon2");
-// var tempContainer1 = $("#tempContainer2");
-// var humidityContainer1 = $("#humidityContainer2");
-
-// // querySelectors 5-Day Forecast: date3
-// var date1 = $("#date3");
-// var icon1 = $("#icon3");
-// var tempContainer1 = $("#tempContainer3");
-// var humidityContainer1 = $("#humidityContainer3");
-
-// // querySelectors 5-Day Forecast: date4
-// var date1 = $("#date4");
-// var icon1 = $("#icon4");
-// var tempContainer1 = $("#tempContainer4");
-// var humidityContainer1 = $("#humidityContainer4");
-
-// // querySelectors 5-Day Forecast: date5
-// var date1 = $("#date5");
-// var icon1 = $("#icon5");
-// var tempContainer1 = $("#tempContainer5");
-// var humidityContainer1 = $("#humidityContainer5");
-
-// // querySelectors 5-Day Forecast: date6
-// var date1 = $("#date6");
-// var icon1 = $("#icon6");
-// var tempContainer1 = $("#tempContainer6");
-// var humidityContainer1 = $("#humidityContainer6");
-
-
 var currentDay = moment().format("MM-DD-YYYY");
 
 // // Load Searched Cities
@@ -76,6 +39,12 @@ var loadSearchedCities = function() {
 var fetchWeather = function(pickedCity) {
     // API URL for Picked City
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather/?q=" + pickedCity + "&appid=86fcb44b6b11593f53514dda5d0a62ae";
+
+    if (location.protocol === 'http:') {
+        apiUrl = "http://api.openweathermap.org/data/2.5/weather/?q=" + pickedCity + "&appid=86fcb44b6b11593f53514dda5d0a62ae";
+    } else {
+        apiUrl = "https://api.openweathermap.org/data/2.5/weather/?q=" + pickedCity + "&appid=86fcb44b6b11593f53514dda5d0a62ae";
+    }
 
     fetch(apiUrl).then(function(response) {
         // check if request was successful
